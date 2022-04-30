73178a0ca55b2ef78cdd0d326e4ef109ec7403ce
Your Name <ashish.baghel@sap.com>
2022-04-30 18:50:07 +0530

    changes

---
+++ Amazon Questions/countPalindromes.js
@@ -0,0 +1,29 @@
+const getCount = (str) => {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    //for single char
+    count += checkCount(str, i, i);
+    count += checkCount(str, i, i + 1);
+  }
+  return count;
+};
+
+const checkCount = (str, leftPointer, rightPointer) => {
+  let count = 0;
+  while (leftPointer >= 0 && rightPointer < str.length) {
+    let newStr = str.substring(leftPointer, rightPointer + 1);
+    if (isPalindrome(newStr)) {
+      //console.log(newStr);
+      count++;
+    }
+    leftPointer--;
+    rightPointer++;
+  }
+  return count;
+};
+
+const isPalindrome = (str) => {
+  return str === str.split("").reverse().join("");
+};
+
+console.log(getCount("abccba"));
