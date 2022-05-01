0a32ed8e074888ebd7311a4affdb7864a0d63ffb
Your Name <ashish.baghel@sap.com>
2022-05-02 00:24:55 +0530

    changes

--- Amazon Questions/longestPalindromRe.js
+++ Amazon Questions/longestPalindromRe.js
@@ -1,6 +1,29 @@
+const rearrange = (str) => {
+  let map = {};
+  for (let i = 0; i < str.length; i++) {
+    if (!map[str[i]]) {
+      map[str[i]] = 1;
+    } else {
+      map[str[i]] += 1;
+    }
+  }
+  let count = 0;
+  let arr = Object.keys(map);
+  let isOdd = false;
+  for (let i = 0; i < arr.length; i++) {
+    if (map[arr[i]] === 1) {
+      isOdd = true;
+    } else {
+      if (map[arr[i]] % 2 === 0) {
+        count = count + 2;
+      } else if (map[arr[i]] > 2) {
+        let num = map[arr[i]] - (map[arr[i]] % 2);
+        count = count + num;
+      }
+    }
+  }
 
-
-
-const rearrange = ()=>{
-
-}
+  if (isOdd) {
+    count = count + 1;
+  }
+};
--- Amazon Questions/replaceWord.js
+++ Amazon Questions/replaceWord.js
@@ -1,5 +1,18 @@
+// Given three strings S, oldW and newW. Find all occurrences of the word oldW in S and replace them with word newW.
+// Example 1:
+// Input:
+// S = "xxforxx xx for xx"
+// oldW = "xx"
+// newW = "Geeks"
+// Output:
+// "geeksforgeeks geeks for geeks"
+// Explanation:
+// Replacing each "xx" with
+// "Geeks" in S.
+// Expected Time Complexity: O(n2)
+// Expected Auxiliary Space: O(1)
+
 const replace = (str, newW, oldW) => {
-  let arr = [];
   let l = 2;
   let result = "";
   let subStr = "";
