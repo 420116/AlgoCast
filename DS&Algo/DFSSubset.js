2d1fff4bc9851d002711a3e03a0507703ec79c40
Your Name <ashish.baghel@sap.com>
2022-05-18 01:28:00 +0530

    Backtracking

---
+++ DS&Algo/DFSSubset.js
@@ -0,0 +1,25 @@
+// get the subset of an Array
+// Input = [1,2,3];
+// Output = [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,3]]
+
+const getSubset = (arr) => {
+  let results = [];
+  let subset = [];
+  DFS(arr, 0, results, subset);
+  console.log(results);
+};
+
+const DFS = (arr, i, results, subset) => {
+  if (i >= arr.length) {
+    results.push([...subset]);
+    return;
+  }
+
+  subset.push(arr[i]);
+  DFS(arr, i + 1, results, subset);
+  subset.pop();
+  DFS(arr, i + 1, results, subset);
+};
+
+getSubset([2, 4]); // output [ [ 2, 4 ], [ 2 ], [ 4 ], [] ]
+getSubset([1, 2, 3]); // output [[ 1, 2, 3 ], [ 1, 2 ],[ 1, 3 ],    [ 1 ], [ 2, 3 ],    [ 2 ],[ 3 ],       []]
---
+++ DS&Algo/rodCut.js
@@ -0,0 +1,36 @@
+// Given a rod of length N inches and an array of prices, price[] that contains prices of all pieces of size smaller than N.
+// Determine the maximum value obtainable by cutting up the rod and selling the pieces.
+// Example 1:
+// Input:
+// N = 8
+// Price[] = {1, 5, 8, 9, 10, 17, 17, 20}
+// Output:
+// 22
+
+//brute Force solution
+const rodCut = (price, N) => {
+  let maxPrice = 0;
+
+  for (let i = 0; i < N; i++) {
+    for (let j = i + 1; j < N; j++) {
+      if (i + j + 2 === N) {
+        maxPrice = Math.max(maxPrice, price[i] + price[j]);
+      }
+    }
+  }
+  console.log(maxPrice);
+};
+
+rodCut([1, 5, 8, 9, 10, 17, 17, 20], 8);
+
+//DFS Solution
+
+const cutRod = (price, N, maxPrice, i) => {
+  if (N >= 0) {
+    return maxPrice;
+  }
+
+  return Math.max(price[i] + cutRod(price, N - 1, maxPrice, i + 1));
+};
+
+console.log(cutRod([1, 5, 8, 9, 10, 17, 17, 20], 8, 0, 0));
