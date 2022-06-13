3db7396496c2a06b99cf13d13ae4efa150788569
ashishbaghel10@gmail.com <ashishbaghel10@gmail.com>
2022-06-13 23:47:39 +0530

    update

--- DS&Algo/subArray.js
+++ DS&Algo/subArray.js
@@ -1,53 +1,41 @@
-function subArraySum(arr, n, s) {
+// Given an unsorted array A of size N that contains only non-negative integers,
+// find a continuous sub-array which adds to a given number S.
+// In case of multiple subarrays, return the subarray which comes first on moving from left to right.
+// Example 1:
+// Input:
+// N = 5, S = 12
+// A[] = {1,2,3,7,5}
+// Output: 2 4
+// Explanation: The sum of elements
+// from 2nd position to 4th position
+// is 12.
+
+const subArraySum = (arr, s) => {
+  let leftPointer = 0;
+  let currPointer = 0;
   let sum = 0;
-  let subArray = [];
-  for (let i = 0; i < arr.length; i++) {
-    sum = arr[i];
+  while (currPointer < arr.length) {
+    sum = sum + arr[currPointer];
+    while (sum > s) {
+      sum = sum - arr[leftPointer];
+      leftPointer++;
+    }
     if (sum === s) {
-      return [i + 1, i + 1];
+      return [leftPointer + 1, currPointer + 1];
     }
-    if (subArray.length > 0) {
-      return subArray;
-    }
-    for (let j = i + 1; j < arr.length; j++) {
-      if (sum > s) {
-        break;
-      }
-      sum = sum + arr[j];
-      if (sum === s) {
-        subArray.push(i + 1);
-        subArray.push(j + 1);
-      }
-    }
+    currPointer++;
   }
-
-  console.log(subArray);
-}
-
-const subArray2 = (arr, sum) => {
-  let pointer = 0;
-  let currentSum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    currentSum = currentSum + arr[i];
-    while (sum < currentSum) {
-      currentSum = currentSum - arr[pointer];
-      //console.log(currentSum, sum);
-      pointer += 1;
-    }
-    if (currentSum === sum) {
-      console.log(pointer, i);
-      return currentSum;
-    }
-  }
-  return -1;
+  return [-1];
 };
 
-subArray2(
-  [
-    26, 144, 125, 24, 173, 62, 182, 4, 33, 106, 194, 126, 32, 93, 143, 23, 87,
-    65,
-  ],
-  528
+console.log(
+  subArraySum(
+    [
+      26, 144, 125, 24, 173, 62, 182, 4, 33, 106, 194, 126, 32, 93, 143, 23, 87,
+      65,
+    ],
+    528
+  )
 );
 
 //subArray2([1, 2, 3, 7, 10, 11], 21);
